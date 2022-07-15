c6763bc4601c858bb17bbd6c6268fdef76f716f6
EZsmail <107932200+EZsmail@users.noreply.github.com>
2022-07-15 16:48:13 +0300

    Update app.js

--- Project/slider-sources/app.js
+++ Project/slider-sources/app.js
@@ -1,36 +1,36 @@
-const item = document.querySelector(".item");
-const placeholders = document.querySelectorAll('.placeholder')
+const upBtn = document.querySelector(".up-button")
+const downBtn = document.querySelector(".down-button")
+const sidebar = document.querySelector(".sidebar")
+const mainSlide = document.querySelector(".main-slide")
+const container = document.querySelector(".container")
+const slidesCount = mainSlide.querySelectorAll("div").length;
 
-item.addEventListener("dragstart", dragstart);
-item.addEventListener("dragend", dragend);
+let activeSlideIndex = 0;
 
-for (const placeholder of placeholders) {
-	placeholder.addEventListener('dragover', dragover)
-	placeholder.addEventListener('dragenter', dragenter)
-	placeholder.addEventListener('dragleave', dragleave)
-	placeholder.addEventListener('drop', dragdrop)
-}
+sidebar.style.top = `-${(slidesCount - 1) * 100}vh`
+upBtn.addEventListener('click', () => {
+  changeSlide('up');
+});
+downBtn.addEventListener('click', () => {
+  changeSlide('down');
+});
+function changeSlide(direction) {
+  if (direction === 'up') {
+    activeSlideIndex++
+    if (activeSlideIndex === slidesCount) {
+      activeSlideIndex = 0
+	  console.log('hui')
+    }
+  } else if (direction === 'down') {
+    activeSlideIndex--
+    if (activeSlideIndex < 0) {
+      activeSlideIndex = slidesCount - 1
+	 console.log('hiu')
+    }
+  }
 
-function dragstart(event) {
-  event.target.classList.add("hold");
-  setTimeout(() => event.target.classList.add("hide"), 0)
-  ;
-}
+  const height = container.clientHeight;
 
-function dragend(event) {
-  event.target.className = "item"
-}
- function dragover(event){
-	event.preventDefault()
- }
-
- function dragenter(event){
-	event.target.classList.add('hovered')
-}
-function dragleave(event){
-	event.target.classList.remove('hovered')
-}
-function dragdrop(event){
-	event.target.append(item)
-	event.target.classList.remove('hovered')
+  mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+  sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
 }
